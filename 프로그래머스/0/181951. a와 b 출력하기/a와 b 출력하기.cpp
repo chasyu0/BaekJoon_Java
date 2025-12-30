0bf6f1b3c45869901d1da19722e2ce23d56e8184
JISU KANG <162910117+chasyu0@users.noreply.github.com>
2025-12-30 22:17:10 +0900

    [level 0] Title: a와 b 출력하기, Time: 2.79 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a;
+    cin >> b;
+    cout << "a = " << a << "\n";
+    cout << "b = " << b << "\n";
+    return 0;
+}
